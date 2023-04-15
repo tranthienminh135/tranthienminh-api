03f87a71e19ca8dc12d941da7b6432e4593647a4
tranthienminh135 <tranthienminh135@gmail.com>
2023-04-15 19:02:12 +0700

    update

--- src/services/PuppeteerService.js
+++ src/services/PuppeteerService.js
@@ -3,8 +3,6 @@ const puppeteer = require("puppeteer");
 const handlePuppeteer = async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    ignoreHTTPSErrors: true,
   });
   const page = await browser.newPage();
   await page.goto("https://www.google.com");
